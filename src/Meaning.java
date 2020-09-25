a60a2d4f8d37040e30cbdbbe617435153a17e74d
Nguyen Vu Hieu <nguyenvuhieu2015@gmail.com>
2020-09-26 00:27:51 +0700

    Update classes to match all input data
    add explanation
    explanation include example
    add phrase to meaning
    add javafx to use Pair

--- src/Meaning.java
+++ src/Meaning.java
@@ -1,15 +1,19 @@
+import javafx.util.Pair;
+
 import java.util.List;
 
 public class Meaning {
     private int id;
-    private List<String> explanation;
+    private List<Explanation> explanation;
+    private List<Pair<String, String>> phrases;
 
     public Meaning() {
     }
 
-    public Meaning(int id, List<String> explanation) {
+    public Meaning(int id, List<Explanation> explanation, List<Pair<String, String>> phrases) {
         this.id = id;
         this.explanation = explanation;
+        this.phrases = phrases;
     }
 
     public int getId() {
@@ -20,11 +24,19 @@ public class Meaning {
         this.id = id;
     }
 
-    public List<String> getExplanation() {
+    public List<Explanation> getExplanation() {
         return explanation;
     }
 
-    public void setExplanation(List<String> explanation) {
+    public void setExplanation(List<Explanation> explanation) {
         this.explanation = explanation;
+    }
+
+    public List<Pair<String, String>> getPhrases() {
+        return phrases;
+    }
+
+    public void setPhrases(List<Pair<String, String>> phrases) {
+        this.phrases = phrases;
     }
 }
